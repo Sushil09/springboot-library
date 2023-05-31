b631b05992e52996fdf8953e23bdb84410ba354b
Sushil <sjsushil09@gmail.com>
2023-05-31 22:35:38 +0530

    Exposing the library application

--- src/main/java/com/sjsuhil09/dao/BookRepository.java
+++ src/main/java/com/sjsuhil09/dao/BookRepository.java
@@ -1,2 +1,7 @@
-package com.sjsuhil09.dao;public interface BookRepository {
+package com.sjsuhil09.dao;
+
+import com.sjsuhil09.entity.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BookRepository extends JpaRepository<Book, Long> {
 }
--- src/main/java/com/sjsuhil09/entity/Book.java
+++ src/main/java/com/sjsuhil09/entity/Book.java
@@ -1,2 +1,30 @@
-package com.sjsuhil09.entity;public class Book {
+package com.sjsuhil09.entity;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title")
+    private String title;
+    @Column(name = "author")
+    private String author;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "copies")
+    private int copies;
+    @Column(name = "copies_available")
+    private int copiesAvaialable;
+    @Column(name = "category")
+    private String category;
+    @Column(name = "img")
+    private String image;
 }
